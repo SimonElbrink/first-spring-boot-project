9a964b33682fd689ae78bc6d47a05f9cb70e8a3a
Simon Elbrink <simon.elbrink@gmail.com>
2021-09-06 11:20:07 +0200

    Created out first Entity. Java Class -> Database Table

---
+++ src/main/java/se/lexicon/firstspringbootproject/model/Student.java
@@ -0,0 +1,27 @@
+package se.lexicon.firstspringbootproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //Need For Creating The Entity.
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
